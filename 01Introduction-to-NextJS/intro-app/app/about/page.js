2fae8f1dc813d75060c4051b4f50b9714863e0dc
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-01-12 15:26:42 +0530

    01Introduction to NextJS code added

---
+++ 01Introduction-to-NextJS/intro-app/app/about/page.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">Problem Solved by NextJS</h1>
+
+      <ul className="text-2xl">
+        <li>Full Stack Solution</li>
+        <li>File based Routing</li>
+        <li>Additional Features like router from next/navigation</li>
+        <li>Optimized Rendering</li>
+      </ul>
+    </div>
+  );
+};
+
+export default page;
